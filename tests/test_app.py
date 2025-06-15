81e91d2cbca08a5be230432da2ce8b778088af79
Wagner Diniz <wagner.diniz.barcellos@gmail.com>
2025-06-15 16:04:30 -0300

    modificação dos exercicios propostos da aula 3

--- tests/test_app.py
+++ tests/test_app.py
@@ -65,3 +65,50 @@ def test_delete_user(client):
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message':'User Deleted'}
+
+def test_update_user_should_return_not_found__exercicio(client):
+    response = client.put(
+        '/users/666',
+        json = {
+            'username':'bob',
+            'email':'bob@example.com',
+            'password':'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail':'User not found'}
+
+def test_delete_user_should_return_not_found__exercicio(client):
+    response = client.delete('/users/666') 
+
+    assert response.status_code == HTTPStatus.NOT_FOUND 
+    assert response.json() == {'detail': 'User not found'}
+
+def test_get_user_should_return_not_found__exercicio(client):
+    response = client.get('/users/666')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_get_user___exercicio(client):
+    # Primeiro, cria o usuário bob
+    client.post(
+        '/users/',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    # Agora sim, busca o usuário com ID 1
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
